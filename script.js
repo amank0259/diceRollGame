45e0052199a1747ff62b96f56cc8cbcc685d1a38
amank0259 <amank0259@gmail.com>
2023-09-07 17:39:31 +0530

    Added New Features

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 "use strict";
 
 //  Selecting Elements
+const player0El = document.querySelector(".player-0");
+const player1El = document.querySelector(".player-1");
 const score0El = document.querySelector("#score-0");
 const score1El = document.getElementById("score-1");
 const diceEl = document.querySelector(".dice");
@@ -15,7 +17,9 @@ score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add("hidden");
 
+const score = [0, 0];
 let currentScore = 0;
+let activePlayer = 0;
 
 // Rolling Dice Function
 btnRoll.addEventListener("click", function () {
@@ -31,8 +35,16 @@ btnRoll.addEventListener("click", function () {
   if (dice !== 1) {
     // Add dice to Current Player
     currentScore += dice;
+    document.getElementById(`current-${activePlayer}`).textContent =
+      currentScore;
+
     current0El.textContent = currentScore;
   } else {
     // Switch to Next Player
+    document.getElementById(`current-${activePlayer}`).textContent = 0;
+    currentScore = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    player0El.classList.toggle("player-active");
+    player1El.classList.toggle("player-active");
   }
 });
