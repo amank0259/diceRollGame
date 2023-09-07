6dc8bd202af3510896935d955d6d9bf091896b79
amank0259 <amank0259@gmail.com>
2023-09-07 17:18:10 +0530

    Update

--- script.js
+++ script.js
@@ -7,11 +7,15 @@ const diceEl = document.querySelector(".dice");
 const btnNew = document.querySelector(".btn-new");
 const btnRoll = document.querySelector(".btn-dice");
 const btnHold = document.querySelector(".btn-hold");
+const current0El = document.querySelector("#current-0");
+const current1El = document.querySelector("#current-1");
 
 // Starting Condition
 score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add("hidden");
+
+let currentScore = 0;
 
 // Rolling Dice Function
 btnRoll.addEventListener("click", function () {
@@ -22,4 +26,13 @@ btnRoll.addEventListener("click", function () {
   // 2.  Display Dice
   diceEl.classList.remove("hidden");
   diceEl.src = `Alea_${dice}.png`;
+
+  //   3. Check for rolled 1: if true, switch to next player
+  if (dice !== 1) {
+    // Add dice to Current Player
+    currentScore += dice;
+    current0El.textContent = currentScore;
+  } else {
+    // Switch to Next Player
+  }
 });
