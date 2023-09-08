0010bf3b7280a2076d06cf0f888d376ae4533623
Aman kumar <94205023+amank0259@users.noreply.github.com>
2023-09-08 09:18:03 +0530

    Bug fix

--- script.js
+++ script.js
@@ -37,8 +37,6 @@ btnRoll.addEventListener("click", function () {
     currentScore += dice;
     document.getElementById(`current-${activePlayer}`).textContent =
       currentScore;
-
-    current0El.textContent = currentScore;
   } else {
     // Switch to Next Player
     document.getElementById(`current-${activePlayer}`).textContent = 0;
